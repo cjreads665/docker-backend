83ac91d0c4450d50195f4453f6a360e0d5a875f7
cjreads665 <shahid.alam57@gmail.com>
2023-03-22 09:49:48 +0545

    added express and morgan

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log(5555);
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+
+
+const app = express();
+
+app.use(morgan('dev'));
+
+
+app.get('/',(req:Request,res:Response)=>{
+    res.json({hello: 'world'});
+})
+
+const port = Number(process.env.PORT ?? 8080)
+app.listen(port,'0.0.0.0',()=>{
+    console.log(`started port at ${port}`);
+    
+})
