769f17ea2e9eb402e89f8473b765618563de6750
cjreads665 <shahid.alam57@gmail.com>
2023-04-06 21:50:57 +0530

    added seed data to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
+import { PrismaClient } from '@prisma/client';
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 
+const db = new PrismaClient({log:['error','info','query','warn']}) //what we want to log
 
+const seedDatabase = async() =>{
+    if((await db.post.count())===0){
+        await db.post.createMany({
+            data: [{
+                id: 'one',
+                slug : 'first-page',
+                title : 'First Page',
+                publishedAt : new Date()
+            },
+            {
+                id: 'two',
+                slug :'draft-post',
+                title: "Drafts"
+            }
+
+        ]
+        })
+    }
+}
+
+seedDatabase()
 const app = express();
 
 app.use(morgan('dev'));
 
 
-app.get('/',(req:Request,res:Response)=>{
+app.get('/', async(req:Request,res:Response)=>{
     // debugger;
-    res.json({hello: 'dsddaadds666addsadddadadasda'});
+    const posts = await db.post.findMany()
+    res.json(posts);
 })
 
 const port = Number(process.env.PORT ?? 8080)
